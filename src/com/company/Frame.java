d1c23709d638dc122986b759c826e137d1e734ed
Kemal Kazanc <keemalkazanc@gmail.com>
2021-01-13 15:39:44 +0100

    exo terminer

--- src/com/company/Frame.java
+++ src/com/company/Frame.java
@@ -1,44 +1,108 @@
 package com.company;
 
 import javax.swing.*;
-import java.awt.*;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
 
 
 public class Frame extends JFrame {
-
-    public Frame (String title){
-    setTitle(title);
-    setVisible(true);
-    setSize(700,500);
-    setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-    setLocationRelativeTo(null);
-    setResizable(true);
-
-    JPanel contentPane = (JPanel) this.getContentPane();
-    contentPane.setLayout(new FlowLayout(FlowLayout.CENTER));
-    contentPane.add (new JButton("Connexion"));
-    contentPane.add (new JTextField(20));
-    contentPane.add(new JPasswordField(20));
-    contentPane.add(new JLabel("Password"));
-    contentPane.add(new JLabel("Pseudo"));
+    private JTextField textField;
+    private JPasswordField passwordField;
+    private JLabel label;
+    private JLabel label1;
 
 
-    this.addWindowListener(new WindowAdapter() {
-        @Override
-        public void windowClosing(WindowEvent e) {
-            super.windowClosing(e);
-            int clickedButton = JOptionPane.showConfirmDialog(Frame.this,
-                    "Êtes vous sur de vouloir quitter ?" , title , JOptionPane.YES_NO_OPTION );
-            if ( clickedButton == JOptionPane.YES_OPTION) {
-                dispose();
-                setVisible(true);
-            }
+    public Frame () {
+        super("Fenêtre de connexion");
+        setVisible(true);
+        setSize(700, 500);
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(true);
+
+        JPanel contentPane = (JPanel) this.getContentPane();
+        contentPane.setLayout(null);
+
+        //Création du boutton + la connexion
+        JButton button = new JButton("connexion");
+        contentPane.add(button);
+        button.setBounds(125,150,200,30);
+        button.addActionListener(this::OnSeConnect);
+
+        textField = new JTextField();
+        textField.setBounds(200,35,200,30);
+        contentPane.add (textField);
+
+        passwordField = new JPasswordField();
+        passwordField.setBounds(200,80,200,30);
+        contentPane.add(passwordField);
 
 
+        label = new JLabel("pseudo");
+        label.setBounds(100,35,200,30);
+        contentPane.add(label);
+
+
+        label1 = new JLabel("Password");
+        label1.setBounds(100,80,200,30);
+        contentPane.add(label1);
+
+        // POPUPfenetre pour fermer la fenetre de connexion
+            this.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    int clickedButton = JOptionPane.showConfirmDialog(Frame.this, "Etes-vous sur de vouloir quitter ?", "Title", JOptionPane.YES_NO_OPTION);
+                    if(clickedButton == JOptionPane.YES_OPTION){
+                        Frame.this.dispose();
+                    }
+                }
+            });
         }
-    });
+        public void OnSeConnect(ActionEvent e) {
 
+            //Connexion à la base de donnée
+            Properties props = new Properties();
+            try (FileInputStream fis = new
+                    FileInputStream("conf.properties")) {
+                props.load(fis);
+            } catch (FileNotFoundException fe) {
+                fe.printStackTrace();
+            } catch (IOException IOe) {
+                IOe.printStackTrace();
+            }
+            String jdbcUrl = props.getProperty("jdbc.url");
+            String jdbcLogIn = props.getProperty("jdbc.login");
+            String jdbcPassword = props.getProperty("jdbc.password");
+
+            String recuplogin = textField.getText();
+            char[] BAP = passwordField.getPassword();
+            String recuppassword = new String(BAP);
+            try {
+                Connection connection = DriverManager.getConnection(jdbcUrl, jdbcLogIn, jdbcPassword);
+                PreparedStatement sql = connection.prepareStatement("SELECT user_login, user_password FROM user where user_login=? and user_password=?");
+                sql.setString(1, recuplogin);
+                sql.setString(2, recuppassword);
+                ResultSet resultSql = sql.executeQuery();
+                if(resultSql.next()){
+                    JOptionPane.showMessageDialog(null,"Authentification réussi");
+                } else{
+                    JOptionPane.showMessageDialog(null,"Authentification échouer ");
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+
+    public static void  main (String[] args) throws Exception {
+        UIManager.setLookAndFeel(new NimbusLookAndFeel());
+        Frame authentification = new Frame();
+        authentification.setVisible(true);
     }
-}
+    }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,43 +1,11 @@
 
 package com.company;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.Properties;
 
 public class Main {
 
-    public static void  main (String[] args) throws FileNotFoundException {
-        new Frame("Connexion");
+    public static void  main (String[] args) {
 
-        Properties props = new Properties ();
-        try (fileInputStream fis = new
-                FileInputStream ("conf.prosperties")) {
-            props.load(fis);
-        }
-        String url = props.getProperty ("jdbc.url");
-        String login = props.getProperty("jdbc.login");
-        String password = props.getProperty("jdbc.password");
-        try (Connection connection =
-        DriverManager.getConnection(url, login, password)) {
-            String strSQL = "SELECT * FROM  limit 10;";
-            try (Statement statement = connection.createStatement() )
-            {
-                ResultSet resultSet =
-                statement.executeQuery(strSQL);
-while (resultSet.next()) { String user_id =
-                resultSet.getString ("user_id");
-                           String user_id =
-                resultSet.getString ("user_login");
-                           String userPassword =
-                resultSet.getString ("user_password"); String user_password =
-
-System.out.printf("%s %s %s \n", user_id, user_login, user_password);
-            } }
-        } }
+    }
 }
 
 
